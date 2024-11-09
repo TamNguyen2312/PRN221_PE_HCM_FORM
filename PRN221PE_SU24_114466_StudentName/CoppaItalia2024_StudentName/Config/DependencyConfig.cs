27521590fa4350bc0a4c2f2b747962033520a78b
TamNguyen2312 <th3m1nd05@gmail.com>
2024-11-10 04:55:01 +0700

    Update DependencyConfig.cs

--- PRN221PE_SU24_114466_StudentName/CoppaItalia2024_StudentName/Config/DependencyConfig.cs
+++ PRN221PE_SU24_114466_StudentName/CoppaItalia2024_StudentName/Config/DependencyConfig.cs
@@ -1,3 +1,6 @@
+using ConsultingKoiFish.DAL.Repositories;
+using ConsultingKoiFish.DAL.UnitOfWork;
+
 namespace CoppaItalia2024_StudentName.Config;
 
 public class DependencyConfig
@@ -5,6 +8,7 @@ public class DependencyConfig
     public static void Register(IServiceCollection services)
     {
         //FS.DAL
-        
+        services.AddTransient(typeof(IRepoBase<>), typeof(RepoBase<>));
+        services.AddScoped(typeof(IUnitOfWork), typeof(UnitOfWork));
     }
 }
